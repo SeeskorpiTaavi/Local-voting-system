85e1e4bd95e8a1766c00e8f7f00e989320e9b09d
SeeskorpiTaavi <36662771+SeeskorpiTaavi@users.noreply.github.com>
2019-05-09 09:43:50 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(e){
-    var siteUrl = document.getElementById('siteUrl').value;
+    var artistVote = document.getElementById('artistVote').value;
 
     var bookmark = {
-        name: siteName,
-        url: siteUrl
+        voting: artistVote
     }
 
-    if(localStorage.getItem('bookmarks') === null){
+    if(localStorage.getItem('artistit') === null){
 
-        var bookmarks = [];
+        var artistit = [];
 
-        bookmarks.push(bookmark);
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        artistit.push(bookmark);
+
+        localStorage.setItem('artistit', JSON.stringify(artistit));
+
+    } else {
+        var artistit = JSON.parse(localStorage.getItem('artistit'));
+        
+        artistit.push(bookmark);
+
+        localStorage.setItem('artistit', JSON.stringify(artistit));
     }
 
     e.preventDefault();
+}
+
+    function fetchartistit(){
+        var artistit = JSON.parse(localStorage.getItem('artistit'));
+
+    var artistitResults = document.getElementById('artistitResults');
+
+    artistitResults.innerHTML = '';
+    for(var i = 0; i < artistit.length; i++){
+        var voting = artistit[i].voting;
+
+    }
+
 }
